a2dc3903ad3fe0024f547d8422a7c608e0f13f92
steven <webber.steven@gmail.com>
2022-12-26 14:57:26 +0200

    Cleanup of unused endpoints and tests

--- src/test/java/za/webber/projects/GreetingTest.java
+++ src/test/java/za/webber/projects/GreetingTest.java
@@ -16,25 +16,4 @@ public class GreetingTest
                 .body(equalTo("hello jaxrs"));
     }
 
-    @Test
-    public void testServlet() {
-        RestAssured.when().get("/servlet/hello").then()
-                .contentType("text/plain")
-                .body(equalTo("hello servlet"));
-    }
-
-    @Test
-    public void testVertx() {
-        RestAssured.when().get("/vertx/hello").then()
-                .contentType("text/plain")
-                .body(equalTo("hello vertx"));
-    }
-
-    @Test
-    public void testFunqy() {
-        RestAssured.when().get("/funqyHello").then()
-                .contentType("application/json")
-                .body(equalTo("\"hello funqy\""));
-    }
-
 }
