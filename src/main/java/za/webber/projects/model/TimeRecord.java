a0d26beae6d192a09f66fb0fd7ac4be4ca2a6b42
Steven Webber <steven.webber@johnlewis.co.uk>
2023-06-23 18:03:33 +0200

    Add id for key

--- src/main/java/za/webber/projects/model/TimeRecord.java
+++ src/main/java/za/webber/projects/model/TimeRecord.java
@@ -5,11 +5,19 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbParti
 
 @DynamoDbBean
 public class TimeRecord {
-
+    private String id;
     private String name;
     private String description;
 
     @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
